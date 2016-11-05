86c9ba071db595592202bd39abf3a45039eba6b7
Renato Zannon <renato@rrsz.com.br>
2016-11-05 20:23:18 -0200

    Add conversation methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
+require 'conversation'
+require 'json'
+
 class User < ApplicationRecord
+  def initial_message
+    query_conversation('')
+  end
+
+  def query_conversation(user_input)
+    response = conversation.send_message(user_input)
+    self.prev_conversation_context = response['context']
+
+    response
+  end
+
+  private
+
+  def conversation
+    return @conversation if defined?(@conversation)
+
+    if prev_conversation_context
+      parsed_context = JSON.parse(prev_conversation_context)
+    else
+      parsed_context = nil
+    end
+
+    @conversation = Conversation.new(parsed_context)
+  end
 end
