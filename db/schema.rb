1b3855e154346f10781a2a8f16ffb8b428c136b6
Renato Zannon <renato@rrsz.com.br>
2016-11-05 19:49:38 -0200

    Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161105214023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "full_name",                    null: false
+    t.string   "email",                        null: false
+    t.integer  "age",                          null: false
+    t.boolean  "age_confirmed", default: true, null: false
+    t.string   "facebook_id",                  null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
 
 end
