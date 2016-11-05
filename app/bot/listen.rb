528b6ee0fe26578d112b06252472284731bf7ced
Camila Campos <camilacamposramos.si@gmail.com>
2016-11-05 21:57:23 +0000

    send message back to user

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -4,5 +4,10 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe
 
 Bot.on :message do |message|
-  puts "got your message!"
+  Bot.deliver(
+    recipient: message.sender,
+    message: {
+      text: message.text
+    }
+  )
 end
