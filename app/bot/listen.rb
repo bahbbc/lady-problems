f94dcf03ad0381fd1b994828d8a58e8ba270b410
Barbara Barbosa <barbarab.claudino@gmail.com>
2016-11-06 02:20:21 -0200

    add query

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -6,6 +6,7 @@ Facebook::Messenger::Subscriptions.subscribe
 Bot.on :message do |message|
   user = User.find_by(facebook_id: message.sender['id'])
   message_service = MessageCreator.new(user, message)
+  user.query_conversation(message.text)
 
   Bot.deliver(
     recipient: message.sender,
@@ -14,12 +15,12 @@ Bot.on :message do |message|
         type: 'template',
         payload: {
           template_type: 'button',
-          text: message_service.message,
+          text: 'Estou mandando tudo para o Watson =o',
           buttons: message_service.buttons
         }
       }
     }
   )
-  AnticonceptionalMessageWorker.perform_in(15.seconds, user.facebook_id)
-  NextMenstruationMessageWorker.perform_in(30.seconds, user.facebook_id)
+  AnticonceptionalMessageWorker.perform_in(3.seconds, user.facebook_id)
+  NextMenstruationMessageWorker.perform_in(5.seconds, user.facebook_id)
 end
