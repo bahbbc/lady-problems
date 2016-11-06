398fe51bb092d4488a9c4f582fe471f6728f8397
Barbara Barbosa <barbarab.claudino@gmail.com>
2016-11-05 22:19:05 -0200

    Save user

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -4,7 +4,9 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe
 
 Bot.on :message do |message|
-  name = FacebookUserRetriever.new.name_by_sender_id(message.sender['id'])
+  facebook_user_retriever = FacebookUserRetriever.new(message.sender['id'])
+  User.where(facebook_id: message.sender['id']).first_or_create(name: facebook_user_retriever.first_name,
+    last_name: facebook_user_retriever.last_name)
 
   Bot.deliver(
     recipient: message.sender,
--- app/services/facebook_user_retriever.rb
+++ app/services/facebook_user_retriever.rb
@@ -1,14 +1,23 @@
 require 'net/http'
 
 class FacebookUserRetriever
-  def name_by_sender_id(sender_id)
-    request_to_facebook(sender_id)['first_name']
+  def initialize(sender_id)
+    @sender_id = sender_id
+    @request = request_to_facebook(sender_id)
+  end
+
+  def first_name
+    @request['first_name']
+  end
+
+  def last_name
+    @request['last_name']
   end
 
   private
 
-  def request_to_facebook(sender_id)
-    uri = URI.parse("https://graph.facebook.com/v2.6/#{sender_id}/?fields=first_name,last_name,gender&access_token=#{ENV.fetch('FB_ACCESS_TOKEN')}")
+  def request_to_facebook
+    uri = URI.parse("https://graph.facebook.com/v2.6/#{@sender_id}/?fields=first_name,last_name,gender&access_token=#{ENV.fetch('FB_ACCESS_TOKEN')}")
     JSON.parse(Net::HTTP.get_response(uri).body)
   end
 end
